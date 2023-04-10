7943c0638e94efa4e916adc9a1e745a7ce93f3a1
Fernando <me@fdorado.com>
2023-04-10 09:36:25 -0400

    Installed CORS.

--- babybliss_project/babybliss_django/settings.py
+++ babybliss_project/babybliss_django/settings.py
@@ -45,11 +45,18 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:3000"
 ]
 
 REST_FRAMEWORK = {
